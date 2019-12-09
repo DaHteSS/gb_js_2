e7540cfc6cbc6856efc0e9b8cd4a3c1b091e096d
Daniil <ldahtessl@gmail.com>
2019-12-09 14:06:40 +0300

    Rewrited code

--- ls1/js/index.js
+++ ls1/js/index.js
@@ -5,16 +5,12 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title = "Some Stuff", price = 0) => {
-    return `<div class="goods__item"><h3 class="goods__title">${title}</h3><p class="goods__price">${price}$</p></div>`;
-};
+const renderGoodsItem = (title = "Some Stuff", price = 0) =>
+    `<div class="goods__item"><h3 class="goods__title">${title}</h3><p class="goods__price">${price}$</p></div>`;
 
 const renderGoodsList = (list = []) => {
-    // Так как innerHTML вставляет массив, а в массиве данные разделены запятой
-    // Поэтому можно добавлять значения в строку и потом уже её вставлять
-    let $values = '';
-    const goodsList = list.map(item => $values += renderGoodsItem(item.title, item.price));
-    return $values;
+    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    document.querySelector('.goods__list').innerHTML = goodsList.join("");
 }
 
-document.querySelector('.goods__list').innerHTML = renderGoodsList(goods);
+renderGoodsList(goods);
